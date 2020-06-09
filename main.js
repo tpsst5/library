06d6934d4e19548d9af114baa5f91dc0af78bc11
Tim Shea <tshea31@gmail.com>
2020-06-08 21:12:25 -0400

    Removed local storage and fixed up the css

--- main.js
+++ main.js
@@ -9,53 +9,50 @@ function Book(book, author, pages, read, index) {
 }
 
 const newBookFunc = () => {
-  const title = document.getElementById('titleInput');
-  const author = document.getElementById('authorInput');
-  const pages = document.getElementById('pagesInput');
-  const wasRead = document.getElementById('wasRead');
-  const notRead = document.getElementById('notRead');
+  const title = document.getElementById('title-input');
+  const author = document.getElementById('author-input');
+  const pages = document.getElementById('pages-input');
+  const read = document.getElementById('read');
+  const notRead = document.getElementById('not-read');
 
-  if(!title.value || 
-    !author.value || 
-    !pages.value || 
-    (wasRead.checked === false && 
-    notRead.checked === false)) return;
+  if (!title.value ||
+    !author.value ||
+    !pages.value ||
+    (read.checked === false &&
+      notRead.checked === false)) return;
 
-  const readOrNot = wasRead.checked ? wasRead.value : notRead.value;
+  const readOrNot = read.checked ? read.value : notRead.value;
   const newBook = new Book(title.value, author.value, pages.value, readOrNot);
   addBookToLibrary(newBook);
 
-  // Reset required alerts
-  // window.alert = '';
-
-  // Reset form inputs 
+  // Reset form inputs //
   title.value = '';
   author.value = '';
   pages.value = '';
-  wasRead.checked = false;
+  read.checked = false;
   notRead.checked = false;
 };
 
-// Event listener for 'Add Book' button
-document.getElementById('addBook').addEventListener('click', newBookFunc);
+// Event listener for 'Add Book' button //
+document.getElementById('add-book').addEventListener('click', newBookFunc);
 
 function addBookToLibrary(book) {
   myLibrary.unshift(book);
   render(myLibrary);
 }
 
-// Change read status
-const changeRead = (e) => { 
+// Change read status //
+const changeRead = (e) => {
   e.target.innerText = e.target.innerText === 'Yes' ? 'No' : 'Yes';
   const targetBook = e.path[2].cells[0].innerHTML;
   myLibrary.filter(index => {
     if (index.book === targetBook) {
       index.read = index.read === 'Yes' ? 'No' : 'Yes';
-    } 
+    }
   });
-} 
+}
 
-// Remove row from table
+// Remove row from table //
 const clearRow = (e) => {
   e.path[2].remove();
   const targetBook = e.path[2].cells[0].innerHTML;
@@ -63,17 +60,17 @@ const clearRow = (e) => {
   myLibrary.splice(bookIndex, 1);
 }
 
-// Inserts book input data to new row
+// Inserts book input data to new row //
 const render = ((inputArr) => {
-  const delBtn = document.createElement('BUTTON');
-  delBtn.innerHTML = 'DEL';
-  delBtn.id = 'delBtn';
+  const deleteBtn = document.createElement('BUTTON');
+  deleteBtn.innerHTML = 'DEL';
+  deleteBtn.id = 'delete-btn';
 
   const readBtn = document.createElement('BUTTON');
   readBtn.innerHTML = inputArr[0].read;
-  readBtn.id = 'readBtn';
+  readBtn.id = 'read-btn';
 
-  const table = document.getElementById('tableBody');
+  const table = document.getElementById('table-body');
   let row;
   row = table.insertRow(0)
   row.insertCell(0).innerHTML = inputArr[0].book;
@@ -81,17 +78,17 @@ const render = ((inputArr) => {
   row.insertCell(2).innerHTML = inputArr[0].pages + ' pages';
 
   row.insertCell(3).append(readBtn);
-  document.getElementById('readBtn').addEventListener('click', changeRead);
+  document.getElementById('read-btn').addEventListener('click', changeRead);
 
-  row.insertCell(4).append(delBtn);
-  document.getElementById('delBtn').addEventListener('click', clearRow);
+  row.insertCell(4).append(deleteBtn);
+  document.getElementById('delete-btn').addEventListener('click', clearRow);
 });
 
-// Clears entire table and myLibrary array
+// Clears entire table and myLibrary array //
 const clearTable = () => {
-  document.getElementById('tableBody').innerHTML = ''; 
+  document.getElementById('table-body').innerHTML = '';
   myLibrary = [];
 }
 
-// Event listener for 'Reset' button
+// Event listener for 'Reset' button //
 document.getElementById('reset').addEventListener('click', clearTable);
